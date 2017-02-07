d5d2d435e0dbe4ab5db7e1dc0b4086834372d6eb
Steve Celius <steve@celius.cc>
2017-02-07 18:10:55 +0100

    Getting ready for a new version

--- src/EPiCode.Commerce.RestApi/Properties/AssemblyInfo.cs
+++ src/EPiCode.Commerce.RestApi/Properties/AssemblyInfo.cs
@@ -1,31 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Developer Rest Service for Episerver Commerce")]
-[assembly: AssemblyDescription("A REST based API for Epiaerver Commerce. See https://github.com/BVNetwork/CommerceRestApi for more information.")]
+[assembly: AssemblyTitle("Developer Rest API for Episerver Commerce")]
+[assembly: AssemblyDescription("A REST based API for Episerver Commerce. See https://github.com/BVNetwork/CommerceRestApi for more information. This is not suitable for production use.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EPiCode")]
-[assembly: AssemblyProduct("Developer Rest Service for Episerver Commerce")]
+[assembly: AssemblyProduct("Developer Rest API for Episerver Commerce")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("65dbfa9b-d96d-48b0-917e-e3f22cc23dbc")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyVersion("2.2.0")]
+[assembly: AssemblyFileVersion("2.2.0")]
--- src/EPiCode.Commerce.RestApi/pack.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-SET CurrentDir=%~dp0
-set version=%1
-if [%1] == [] set version=1.0.0
-nuget pack "%CurrentDir%EPiCode.Commerce.RestApi.nuspec" -Version %version%
-
